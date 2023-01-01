de8de05cbdaa831ceee686abebe56e8e8decf2c4
QI Lin <qilin@ouc.edu.cn>
2023-01-01 09:51:14 +0800

    add negative sign in accx, accy, accz tranfer functions

--- private/accy.m
+++ private/accy.m
@@ -3,4 +3,4 @@ function trm=accy(ffreqs,dirs,wns,z,depth)
 Kz=cosh(z*wns)./sinh(depth*wns);
 %include a maximum cuttoff for the velocity response function
 Kz(find(Kz<0.1))=0.1;
-trm=i*(ffreqs.*ffreqs.*Kz)*sin(dirs);
+trm=-i*(ffreqs.*ffreqs.*Kz)*sin(dirs);
